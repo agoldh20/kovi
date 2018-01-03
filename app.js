20fe056799c4de1da2c52650e3634f6878967ac1
agoldh20 <dmgldwtr@gmail.com>
2018-01-03 09:32:56 -0600

    changed app.js file

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
-var server = app.listen(process.env.PORT || 5000, function () {
-  var port = server.address().port;
-  console.log("Express is working on port " + port);
-});
+app.listen(process.env.PORT || 3000);
